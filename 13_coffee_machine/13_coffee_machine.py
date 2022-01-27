bd296b2cea84a9df7954324af8a1ad3e3b048dfc
heesoolim <heesoo474@gmail.com>
2022-01-27 09:28:59 -0500

    imported classes and cleaned up the code

--- 13_coffee_machine/13_coffee_machine.py
+++ 13_coffee_machine/13_coffee_machine.py
@@ -5,97 +5,40 @@
 # ---------------------------------------------------------------------------
 
 import data
-import os
-
-# function to clear the console
-clear = lambda: os.system('clear')
-
-curr_money = 0
-curr_water = data.resources['water']
-curr_milk = data.resources['milk']
-curr_coffee = data.resources['coffee']
-
-menu = data.MENU
-
-
-def is_res_enough(resource_name, curr_resource, resource_to_compare):
-    """Check if current resource is greater than required resource"""
-    if curr_resource < resource_to_compare:
-        print(f"There is not enough {resource_name}.")
-        return False
-    return True
-
-
-def insert_coins():
-    """Ask the user the number of coins inserted and return the paid amount"""
-    toonies = int(input("\nHow many toonies are you inserting?"))
-    loonies = int(input("How many loonies are you inserting?"))
-    quarters = int(input("How many quarters are you inserting?"))
-    dimes = int(input("How many dimes are you inserting?"))
-
-    return (2 * toonies) + loonies + (0.25 * quarters) + (dimes * 0.1)
-
-
-def process_coin(_money_paid, _price):
-    """Compare the paid amount and cost and return True if paid amount is enough"""
-    print(f"\nPaid    : ${_money_paid}")
-    print(f"Price   : ${_price}")
-    if _money_paid >= _price:
-        print(f"Change  : ${round(_money_paid - _price, 2)}")
-        return True
-    elif _money_paid < _price:
-        print("Please insert enough money. Money refunded")
-        return False
-
+import menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 more_drink = True
 
 while more_drink:
-    clear()
+    # clear()
     print(data.ascii)
     print("What would you like to have? ")
-    coffee_type = input(f"""  1. espresso (${menu['espresso']['cost']})
-  2. latte (${menu['latte']['cost']})
-  3. cappuccino (${menu['cappuccino']['cost']})
-Type the number you want: """)
 
-    if coffee_type == "off":
+    store_menu = menu.Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+    menu_name = input(store_menu.get_items())
+
+    if menu_name == "off":
         print("machine turned off.")
         more_drink = False
-    elif coffee_type == "report":
-        print(f"Water  : {curr_water}ml")
-        print(f"Milk   :  {curr_milk}ml")
-        print(f"Coffee : {curr_coffee}g")
-        print(f"Money  : ${curr_money}")
+    elif menu_name == "report":
+        coffee_maker.report()
+        money_machine.report()
         more_drink = False
     else:
-        req_water = 0
-        req_milk = 0
-        req_coffee = 0
-        if coffee_type == '1':
-            req_water = menu["espresso"]["ingredients"]["water"]
-            req_coffee = menu["espresso"]["ingredients"]["coffee"]
-            cost = menu["espresso"]["cost"]
-        elif coffee_type == '2':
-            req_water = menu["latte"]["ingredients"]["water"]
-            req_milk = menu["latte"]["ingredients"]["milk"]
-            req_coffee = menu["latte"]["ingredients"]["coffee"]
-            cost = menu["latte"]["cost"]
-        elif coffee_type == '3':
-            req_water = menu["cappuccino"]["ingredients"]["water"]
-            req_milk = menu["cappuccino"]["ingredients"]["milk"]
-            req_coffee = menu["cappuccino"]["ingredients"]["coffee"]
-            cost = menu["cappuccino"]["cost"]
+        menu_item = store_menu.find_drink(menu_name)
+        if menu_item is None:
+            print("Menu not found.")
+            break
+        if coffee_maker.is_resource_sufficient(menu_item):
+            print(f"{menu_item.name} is ${menu_item.cost}")
+            if money_machine.make_payment(menu_item.cost):
+                coffee_maker.make_coffee(menu_item)
+            else:
+                print("money is not sufficient.\nPaid:")
+                break
 
-        if is_res_enough("water", curr_water, req_water) and is_res_enough("milk", curr_milk,
-                                                                           req_milk) and is_res_enough("coffee",
-                                                                                                       curr_coffee,
-                                                                                                       req_coffee):
-            money_paid = insert_coins()
-            if process_coin(money_paid, cost):
-                curr_money += cost
-                curr_water -= req_water
-                curr_milk -= req_milk
-                curr_coffee -= req_coffee
-                print("\nHere is your drink. Enjoy!")
-                more_drink = input("do you want more drink? Type 'y' to get more drink: ") == 'y'
+            more_drink = input("do you want more drink? Type 'y' to get more drink: ") == 'y'
